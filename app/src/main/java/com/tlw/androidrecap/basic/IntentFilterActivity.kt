f98590ea8a60b5f5e26a98eb9918ca5636962e5d
Ashif Ali <84494015+ashifali3147@users.noreply.github.com>
2025-11-27 15:52:15 +0530

    feat: added IntentFilterActivity.kt for exploring intent-filter

---
+++ app/src/main/java/com/tlw/androidrecap/basic/IntentFilterActivity.kt
@@ -0,0 +1,21 @@
+package com.tlw.androidrecap.basic
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.tlw.androidrecap.R
+
+class IntentFilterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_intent_filter)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
