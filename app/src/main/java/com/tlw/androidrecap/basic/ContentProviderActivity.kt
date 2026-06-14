9ff7657af8b07fc74692d4818e3227b878f71647
Ashif Ali <84494015+ashifali3147@users.noreply.github.com>
2026-06-14 11:47:37 +0530

    feat: implement MediaStore query for images in ContentProviderActivity

--- app/src/main/java/com/tlw/androidrecap/basic/ContentProviderActivity.kt
+++ app/src/main/java/com/tlw/androidrecap/basic/ContentProviderActivity.kt
@@ -1,6 +1,9 @@
 package com.tlw.androidrecap.basic
 
+import android.content.ContentUris
+import android.icu.util.Calendar
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,11 +14,48 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.tlw.androidrecap.basic.model.ContentImage
 import com.tlw.androidrecap.basic.ui.theme.AndroidRecapTheme
 
 class ContentProviderActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val projection = arrayOf(
+            MediaStore.Images.Media._ID,
+            MediaStore.Images.Media.DISPLAY_NAME
+        )
+
+        val millisYesterday = Calendar.getInstance().apply {
+            add(Calendar.DAY_OF_YEAR, -1)
+        }.timeInMillis
+        val selection = "${MediaStore.Images.Media.DATE_TAKEN} >= ?"
+        val selectionArgs = arrayOf(
+            millisYesterday.toString()
+        )
+        val sortOrder = "${MediaStore.Images.Media.DATE_TAKEN} DESC"
+
+        contentResolver.query(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            projection,
+            selection,
+            selectionArgs,
+            sortOrder
+        )?.use { cursor ->
+            val idColumn = cursor.getColumnIndex(MediaStore.Images.Media._ID)
+            val displayNameColumn = cursor.getColumnIndex(MediaStore.Images.Media.DISPLAY_NAME)
+
+            val images = mutableListOf<ContentImage>()
+
+            while (cursor.moveToNext()) {
+                val id = cursor.getLong(idColumn)
+                val name = cursor.getString(displayNameColumn)
+                val uri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+
+                images.add(ContentImage(id, name, uri))
+            }
+        }
+
         enableEdgeToEdge()
         setContent {
             AndroidRecapTheme {
